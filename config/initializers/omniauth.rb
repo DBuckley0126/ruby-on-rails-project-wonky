e259d6773612cec6cd0fba487235fa73a1a835c2
DBuckley0126 <dannyboy.msn@hotmail.com>
2019-12-12 00:54:57 +0000

    Fix omniauth bug

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, Rails.application.credentials.google_auth_api_client_id, Rails.application.credentials.google_auth_api_client_secret
+  provider :google_oauth2, Rails.application.credentials.google_auth_api_client_id, Rails.application.credentials.google_auth_api_client_secret, skip_jwt: true
   {    :name => "google",
     :scope => ['contacts', 'plus.login', 'plus.me', 'email',
                'profile'],    
